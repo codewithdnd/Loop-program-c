b5f977f6b322e12231e02e817624f6874197ca29
codewithdnd <100120285+codewithdnd@users.noreply.github.com>
2022-02-24 20:32:08 +0530

    Add files via upload

---
+++ 08_do_while_loop.c
@@ -0,0 +1,14 @@
+#include <conio.h>
+#include <stdio.h>
+ 
+int main ()
+{
+   int a = 1;
+   do {
+      printf("value of a: %d \n", a);
+      a = a + 1;
+   }
+   while( a < 15);
+   return 0;
+getch();
+}
---
+++ 09_while_loop.c
@@ -0,0 +1,14 @@
+#include <conio.h>
+#include <stdio.h>
+ 
+int main () 
+{
+   int a = 1;
+   while( a < 15) {
+      printf("value of a: %d \n", a);
+      a++;
+   }
+ 
+   return 0;
+   getch();
+}
